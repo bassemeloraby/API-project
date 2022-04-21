cad04a63b0ce814559c99b96e729dbe971d3024b
bassem eloraby <BASEM12683@GMAIL.COM>
2022-04-21 07:01:30 +0300

    insomnia

--- app.js
+++ app.js
@@ -1,7 +1,12 @@
 const express = require('express');
+// const res = require('express/lib/response');
 const mongoose = require('mongoose');
 
 app = express();
+
+app.get('/',(req,res)=>{
+    res.json({message:"hello world"})
+})
 
 app.listen(3030,()=>{
     console.log('server is started at port: 3030')
